4f5ae8ac01e91cb65df0cba08eee7474c51c690f
stk346 <stk346@gmail.com>
2024-10-16 05:15:42 +0900

    feat: MovieDto - dto 반환 시 leadactorDto 설정해서 반환하도록 코드 변경

---
+++ src/main/java/com/polar_moviechart/movieservice/domain/service/MovieLeadactorDto.java
@@ -0,0 +1,11 @@
+package com.polar_moviechart.movieservice.domain.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MovieLeadactorDto {
+    private final Integer code;
+    private final String name;
+}
