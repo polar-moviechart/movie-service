4aff5e3f5e6e2999c5ac601b3c6c201a706f7881
stk346 <stk346@gmail.com>
2024-11-11 06:51:35 +0900

    feat: 영화 평가 기능 구현중

--- src/main/java/com/polar_moviechart/movieservice/domain/controller/secureapi/MovieControllerSecure.java
+++ src/main/java/com/polar_moviechart/movieservice/domain/controller/secureapi/MovieControllerSecure.java
@@ -1,24 +1,38 @@
 package com.polar_moviechart.movieservice.domain.controller.secureapi;
 
 import com.polar_moviechart.movieservice.domain.service.MovieRatingCommandService;
+import com.polar_moviechart.movieservice.domain.service.MovieRatingQueryService;
 import com.polar_moviechart.movieservice.utils.CustomResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/secure/api/v1/movies")
 public class MovieControllerSecure {
 
     private final MovieRatingCommandService movieRatingCommandService;
+    private final MovieRatingQueryService movieRatingQueryService;
+
+    @GetMapping("/{code}/rating")
+    public ResponseEntity<CustomResponse<Double>> getMovieRating(HttpServletRequest request,
+                                       @PathVariable(name = "code") int code) {
+        Long userId = (Long) request.getAttribute("userId");
+        Double movieRating = movieRatingQueryService.getUserMovieRating(code, userId);
+
+        return ResponseEntity.ok(new CustomResponse<>(movieRating));
+    }
 
     @PostMapping("/{code}/rating")
-    public ResponseEntity updateRating(HttpServletRequest request,
+    public ResponseEntity<CustomResponse<Double>> updateRating(HttpServletRequest request,
                                        @PathVariable(name = "code") int code,
                                        @RequestBody UpdateRatingRequest updateRatingRequest) {
-        Long userId = (Long) request.getAttribute("userId");
+        Long userId = (Long) request.getSession().getAttribute("userId");
+        log.info("=== MovieController userId = {} ===", userId);
         double ratingValue = movieRatingCommandService.updateRating(code, userId, updateRatingRequest);
 
         return ResponseEntity.ok(new CustomResponse<>(ratingValue));
