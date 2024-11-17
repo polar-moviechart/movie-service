ffc5f95ac6b53b0831f793c6a107809232ad56fb
stk346 <stk346@gmail.com>
2024-11-18 05:06:59 +0900

    infra: commit-stage.yml - 테스트 수행할 때 DB 정보 설정 추가

--- src/test/java/com/polar_moviechart/movieservice/domain/service/MovieDailyStatsQueryServiceTest.java
+++ src/test/java/com/polar_moviechart/movieservice/domain/service/MovieDailyStatsQueryServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
