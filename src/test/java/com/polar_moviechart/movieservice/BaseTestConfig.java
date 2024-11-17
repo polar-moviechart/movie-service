5b6d6557e068e6e6f0fad7faef0376c33c006ddc
stk346 <stk346@gmail.com>
2024-11-18 07:06:13 +0900

    infra: git action 테스트

--- src/test/java/com/polar_moviechart/movieservice/BaseTestConfig.java
+++ src/test/java/com/polar_moviechart/movieservice/BaseTestConfig.java
@@ -9,11 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 @ActiveProfiles("test")
 public abstract class BaseTestConfig {
 
