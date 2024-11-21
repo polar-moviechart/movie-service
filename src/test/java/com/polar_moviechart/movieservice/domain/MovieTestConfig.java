bba64e3a754837c7cd7f6a9f0a39a7685339dc5a
stk346 <stk346@gmail.com>
2024-11-21 12:21:34 +0900

    feat: movie도메인 테스트에서 통합 테스트로 사용할 기본 환경 세팅

--- src/test/java/com/polar_moviechart/movieservice/domain/MovieTestConfig.java
+++ src/test/java/com/polar_moviechart/movieservice/domain/MovieTestConfig.java
@@ -1,9 +1,6 @@
-package com.polar_moviechart.movieservice;
+package com.polar_moviechart.movieservice.domain;
 
 import com.polar_moviechart.movieservice.domain.entity.Movie;
-import com.polar_moviechart.movieservice.domain.entity.MovieDailyStat;
-import com.polar_moviechart.movieservice.domain.repository.MovieDailyStatsRepository;
-import com.polar_moviechart.movieservice.domain.repository.MovieRatingRepository;
 import com.polar_moviechart.movieservice.domain.repository.MovieRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +14,18 @@ import java.util.List;
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
-public abstract class BaseTestConfig {
+public abstract class MovieTestConfig {
 
     @Autowired
-    private MovieRepository movieRepository;
-
-    @Autowired
-    private MovieRatingRepository movieRatingRepository;
-
-    @Autowired
-    private MovieDailyStatsRepository dailyStatsRepository;
+    protected MovieRepository movieRepository;
 
     @BeforeEach
-    void setUpData() {
-        List<Integer> movieCodes = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    void setUpMovies() {
+        List<Integer> movieCodes = List.of(1, 2, 3, 4, 5);
         int year = 2004;
         for (int movieCode : movieCodes) {
             LocalDate releaseDate = LocalDate.of(year, 1, 1);
-            Movie savedMovie = movieRepository.save(
+            movieRepository.save(
                     Movie.builder()
                             .code(movieCode)
                             .title("title" + movieCode)
@@ -43,18 +34,6 @@ public abstract class BaseTestConfig {
                             .productionYear(year)
                             .synopsys("synopsys" + movieCode)
                             .build());
-
-            // MovieDailyStat
-            for (int i = 1; i <= 10; i++) {
-                dailyStatsRepository.save(
-                        MovieDailyStat.builder()
-                        .ranking(i)
-                        .revenue(i * 100)
-                        .date(releaseDate.plusDays(i))
-                        .audience(i * 100)
-                        .movie(savedMovie)
-                        .build());
-            }
         }
     }
 }
