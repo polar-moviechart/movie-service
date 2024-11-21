3d09401e35c689959de427499d5a0035473e71b0
stk346 <stk346@gmail.com>
2024-11-21 17:12:55 +0900

    refactor: MovieDailyStatsRepositoryTest   - static 필드 추가   - static 필드 정보로 필요한 데이터 생성하도록 코드 수정   - 테스트 시 static 필드로 검증하도록 코드 수정

--- src/test/java/com/polar_moviechart/movieservice/domain/repository/MovieDailyStatsRepositoryTest.java
+++ src/test/java/com/polar_moviechart/movieservice/domain/repository/MovieDailyStatsRepositoryTest.java
@@ -3,6 +3,7 @@ package com.polar_moviechart.movieservice.domain.repository;
 import com.polar_moviechart.movieservice.domain.MovieStatsTestConfig;
 import com.polar_moviechart.movieservice.domain.entity.MovieDailyStat;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.Page;
@@ -15,27 +16,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MovieDailyStatsRepositoryTest extends MovieStatsTestConfig {
 
+    private final static List<Integer> movieCodes = List.of(1);
+    private final static LocalDate releaseDate = LocalDate.of(2004, 1, 1);
+    private final static int statCnt = 1;
+    private final static int days = 2;
+
+    @BeforeAll
+    void setUp() {
+        initMovies(movieCodes, releaseDate);
+        initStat(statCnt, days);
+    }
+
     @DisplayName("findAllByDate() 쿼리 테스트")
     @Test
     void findAllByDate() {
         // given
-        LocalDate targetDate = LocalDate.of(2004, 1, 1);
         PageRequest pageRequest = PageRequest.of(0, 10);
         // when
-        Page<MovieDailyStat> movieDailyStatsPage = dailyStatsRepository.findAllByDate(targetDate, pageRequest);
+        Page<MovieDailyStat> movieDailyStatsPage = dailyStatsRepository.findAllByDate(releaseDate, pageRequest);
         // then
-        assertThat(movieDailyStatsPage.getContent()).isNotEmpty();
+        assertThat(movieDailyStatsPage.getContent().size()).isEqualTo(statCnt);
     }
 
     @DisplayName("findByMovieCodeOrderByDateDesc() 쿼리 테스트")
     @Test
     void findByMovieCodeOrderByDateDesc() {
         // given
-        int movieCode = 1;
-        PageRequest pageRequest = PageRequest.of(0, 10);
+        PageRequest pageRequest = PageRequest.of(0, statCnt);
         // when
-        List<MovieDailyStat> dailyStats = dailyStatsRepository.findByMovieCodeOrderByDateDesc(movieCode, pageRequest);
+        List<MovieDailyStat> dailyStats = dailyStatsRepository
+                .findByMovieCodeOrderByDateDesc(movieCodes.get(0), pageRequest);
         // then
-        Assertions.assertThat(dailyStats).isNotEmpty();
+        Assertions.assertThat(dailyStats.size()).isEqualTo(statCnt);
     }
 }
