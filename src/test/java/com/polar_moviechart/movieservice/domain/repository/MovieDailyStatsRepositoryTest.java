303543e0bccc25003226023b7b5b79fd0184a178
stk346 <stk346@gmail.com>
2024-11-22 11:04:56 +0900

    refactor: MovieDailyStatsRepositoryTest   - 테스트 추가   - MovieStatsTestConfig 코드에 맞게 테스트코드 수정

--- src/test/java/com/polar_moviechart/movieservice/domain/repository/MovieDailyStatsRepositoryTest.java
+++ src/test/java/com/polar_moviechart/movieservice/domain/repository/MovieDailyStatsRepositoryTest.java
@@ -18,13 +18,13 @@ class MovieDailyStatsRepositoryTest extends MovieStatsTestConfig {
 
     private final static List<Integer> movieCodes = List.of(1);
     private final static LocalDate releaseDate = LocalDate.of(2004, 1, 1);
-    private final static int statCnt = 1;
+    private final static int stat = 1;
     private final static int days = 2;
 
     @BeforeEach
     void setUp() {
         initMovies(movieCodes, releaseDate);
-        initStat(statCnt, days);
+        initStat(stat, days);
     }
 
     @DisplayName("findAllByDate() 쿼리 테스트")
@@ -35,18 +35,28 @@ class MovieDailyStatsRepositoryTest extends MovieStatsTestConfig {
         // when
         Page<MovieDailyStat> movieDailyStatsPage = dailyStatsRepository.findAllByDate(releaseDate, pageRequest);
         // then
-        assertThat(movieDailyStatsPage.getContent().size()).isEqualTo(statCnt);
+        assertThat(movieDailyStatsPage.getContent().size()).isEqualTo(movieCodes.size());
     }
 
     @DisplayName("findByMovieCodeOrderByDateDesc() 쿼리 테스트")
     @Test
     void findByMovieCodeOrderByDateDesc() {
         // given
-        PageRequest pageRequest = PageRequest.of(0, statCnt);
+        int movieCnt = movieCodes.size();
+        PageRequest pageRequest = PageRequest.of(0, movieCnt);
         // when
         List<MovieDailyStat> dailyStats = dailyStatsRepository
                 .findByMovieCodeOrderByDateDesc(movieCodes.get(0), pageRequest);
         // then
-        Assertions.assertThat(dailyStats.size()).isEqualTo(statCnt);
+        assertThat(dailyStats.size()).isEqualTo(movieCnt);
+    }
+
+    @DisplayName("DB에 저장된 최신 영화 정보를 가져올 수 있다.")
+    @Test
+    void findFirstByOrderByDateDesc() {
+        // given // when
+        MovieDailyStat movieDailyStat = dailyStatsRepository.findFirstByOrderByDateDesc();
+        // then
+        Assertions.assertThat(movieDailyStat.getDate()).isEqualTo(releaseDate.plusDays(days));
     }
 }
