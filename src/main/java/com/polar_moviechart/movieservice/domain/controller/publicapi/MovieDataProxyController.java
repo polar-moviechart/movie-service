35a3fff92ae3f78ecb76e877621ebb873146b180
stk346 <stk346@gmail.com>
2024-11-27 04:43:02 +0900

    feat: MovieDataProxyController - user-service로부터 좋아요, 평점, 리뷰를 가져오는 추상컨트롤러 구현

---
+++ src/main/java/com/polar_moviechart/movieservice/domain/controller/publicapi/MovieDataProxyController.java
@@ -0,0 +1,23 @@
+package com.polar_moviechart.movieservice.domain.controller.publicapi;
+
+import com.polar_moviechart.movieservice.domain.dto.ReviewResponse;
+import com.polar_moviechart.movieservice.utils.CustomResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/public/api/v1/movies")
+public abstract class MovieDataProxyController {
+
+    @GetMapping("/{code}/likes")
+    public abstract ResponseEntity<CustomResponse<Integer>> getLikes(@PathVariable(name = "code") int code);
+
+    @GetMapping("/{code}/reviews")
+    public abstract ResponseEntity<CustomResponse<List<ReviewResponse>>> getReviews(@PathVariable(name = "code") int code);
+
+    @GetMapping("/{code}/ratings")
+    public abstract ResponseEntity<CustomResponse<Double>> getRatings(@PathVariable(name = "code") int code);
+}
