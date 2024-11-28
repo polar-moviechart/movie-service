2e38dae9c8b67a2d16664ea4fa0d4474c23037df
stk346 <stk346@gmail.com>
2024-11-28 10:45:13 +0900

    refactor: 서비스 레이어 리팩토링   - Movie 관련 서비스를 MovieCommandService, MovieQueryService로 통합

--- src/main/java/com/polar_moviechart/movieservice/domain/controller/secureapi/MovieControllerSecure.java
+++ src/main/java/com/polar_moviechart/movieservice/domain/controller/secureapi/MovieControllerSecure.java
@@ -1,7 +1,7 @@
 package com.polar_moviechart.movieservice.domain.controller.secureapi;
 
-import com.polar_moviechart.movieservice.domain.service.MovieRatingCommandService;
-import com.polar_moviechart.movieservice.domain.service.MovieRatingQueryService;
+import com.polar_moviechart.movieservice.domain.service.movie.MovieCommandService;
+import com.polar_moviechart.movieservice.domain.service.movie.MovieQueryService;
 import com.polar_moviechart.movieservice.utils.CustomResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/secure/api/v1/movies")
 public class MovieControllerSecure {
-
-    private final MovieRatingCommandService movieRatingCommandService;
-    private final MovieRatingQueryService movieRatingQueryService;
+    private final MovieCommandService movieCommandService;
+    private final MovieQueryService movieQueryService;
 
     @GetMapping("/{code}/rating")
     public ResponseEntity<CustomResponse<Double>> getMovieRating(HttpServletRequest request,
                                        @PathVariable(name = "code") int code) {
         Long userId = (Long) request.getAttribute("userId");
-        Double movieRating = movieRatingQueryService.getUserMovieRating(code, userId);
+        Double movieRating = movieQueryService.getUserMovieRating(code, userId);
 
         return ResponseEntity.ok(new CustomResponse<>(movieRating));
     }
@@ -33,7 +32,7 @@ public class MovieControllerSecure {
                                        @RequestBody UpdateRatingRequest updateRatingRequest) {
         String userIdString = request.getHeader("X-User-Id");
         Long userId = Long.valueOf(userIdString);
-        double ratingValue = movieRatingCommandService.updateRating(code, userId, updateRatingRequest);
+        double ratingValue = movieCommandService.updateRating(code, userId, updateRatingRequest);
 
         return ResponseEntity.ok(new CustomResponse<>(ratingValue));
     }
