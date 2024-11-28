26114b4b5c6a96b0fd1f11b1af9b451e7d1acedf
stk346 <stk346@gmail.com>
2024-11-28 12:39:57 +0900

    refactor: MovieDetailsDto - dto 반환 메서드에서 좋아요, 평점 포함해서 반환하도록 코드 수정

--- src/main/java/com/polar_moviechart/movieservice/domain/service/dtos/MovieDetailsDto.java
+++ src/main/java/com/polar_moviechart/movieservice/domain/service/dtos/MovieDetailsDto.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class MovieDetailsDto {
     private int code;
     private String title;
+    private Integer likeCnt;
+    private Double rating;
     private List<String> poster;
     private String details;
     private LocalDate releaseDate;
@@ -21,9 +23,11 @@ public class MovieDetailsDto {
     private List<MovieLeadactorDto> movieLeadactorDtos;
 
     @Builder
-    public MovieDetailsDto(int code, String title, String details, LocalDate releaseDate, Integer productionYear, String synopsys, List<MovieDirectorDto> movieDirectorDtos, List<MovieLeadactorDto> movieLeadactorDtos) {
+    public MovieDetailsDto(int code, String title, Integer likeCnt, Double rating, String details, LocalDate releaseDate, Integer productionYear, String synopsys, List<MovieDirectorDto> movieDirectorDtos, List<MovieLeadactorDto> movieLeadactorDtos) {
         this.code = code;
         this.title = title;
+        this.likeCnt = likeCnt;
+        this.rating = rating;
         this.details = details;
         this.releaseDate = releaseDate;
         this.productionYear = productionYear;
@@ -33,15 +37,17 @@ public class MovieDetailsDto {
     }
 
     public static MovieDetailsDto from(Movie movie) {
-        return new MovieDetailsDto(
-                movie.getCode(),
-                movie.getTitle(),
-                movie.getDetails(),
-                movie.getReleaseDate(),
-                movie.getProductionYear(),
-                movie.getSynopsys(),
-                MovieDirectorDto.listFrom(movie.getDirectors()),
-                MovieLeadactorDto.listFrom(movie.getLeadactors())
-        );
+        return MovieDetailsDto.builder()
+                .code(movie.getCode())
+                .title(movie.getTitle())
+                .likeCnt(movie.getLikeCnt())
+                .rating(movie.getRating())
+                .details(movie.getDetails())
+                .releaseDate(movie.getReleaseDate())
+                .productionYear(movie.getProductionYear())
+                .synopsys(movie.getSynopsys())
+                .movieDirectorDtos(MovieDirectorDto.listFrom(movie.getDirectors()))
+                .movieLeadactorDtos(MovieLeadactorDto.listFrom(movie.getLeadactors()))
+                .build();
     }
 }
