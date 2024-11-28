f763afc866cec67081efa3b6764962ad084b43c4
stk346 <stk346@gmail.com>
2024-11-28 09:50:48 +0900

    feat: rabbitMq 관련 코드 추가

--- src/main/java/com/polar_moviechart/movieservice/config/RabbitMqConfig.java
+++ src/main/java/com/polar_moviechart/movieservice/config/RabbitMqConfig.java
@@ -1,14 +1,24 @@
 package com.polar_moviechart.movieservice.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
 public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue userServiceQueue() {
+        return new Queue(queueName, true);
+    }
 
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
