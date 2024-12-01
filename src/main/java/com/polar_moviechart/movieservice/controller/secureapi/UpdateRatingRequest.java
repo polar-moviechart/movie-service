fc9c552e34e525f4f092f07e0ea3abf9be287da2
stk346 <stk346@gmail.com>
2024-12-02 05:18:35 +0900

    refactor: UpdateRatingRequest - 잭슨 라이브러리에서 객체를 생성할 수 있도록 기본 생성자 추가

--- src/main/java/com/polar_moviechart/movieservice/controller/secureapi/UpdateRatingRequest.java
+++ src/main/java/com/polar_moviechart/movieservice/controller/secureapi/UpdateRatingRequest.java
@@ -1,10 +1,13 @@
 package com.polar_moviechart.movieservice.controller.secureapi;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateRatingRequest {
-    private final Double rating;
+    private Double rating;
 }
